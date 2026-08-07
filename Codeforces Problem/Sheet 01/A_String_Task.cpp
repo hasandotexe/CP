fabd7b170d763492aee7350e0b1befebf4ddf499
Hamim__Hasan <hamimlimon12@gmail.com>
2026-08-07 19:40:54 +0600

    Update A_String_Task.cpp

--- Codeforces Problem/Sheet 01/A_String_Task.cpp
+++ Codeforces Problem/Sheet 01/A_String_Task.cpp
@@ -7,7 +7,7 @@ int main ()
     transform(s.begin(), s.end(), s.begin(), ::tolower);
     for(int i=0;i<s.length();i++)
     {
-        if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'){
+        if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='y'){
             continue;
         }
         else{

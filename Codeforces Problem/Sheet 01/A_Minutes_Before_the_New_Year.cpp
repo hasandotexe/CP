4fcf4c3e3041290078a586ba0042dc5676db92ea
Hamim__Hasan <hamimlimon12@gmail.com>
2026-08-21 20:56:02 +0600

    Sheet 01 -A_Minutes_Before_the_New_Year

---
+++ Codeforces Problem/Sheet 01/A_Minutes_Before_the_New_Year.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int h, m;
+        cin >> h >> m;
+        int total = h * 60 + m;
+        cout << (1440 - total) << "\n";
+    }
+    return 0;
+}

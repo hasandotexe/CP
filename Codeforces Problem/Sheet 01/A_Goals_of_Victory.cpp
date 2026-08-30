8816ed7c5a0e32450edb0a9b1c4029ab1b7d58c1
Hamim__Hasan <hamimlimon12@gmail.com>
2026-08-30 18:31:17 +0600

    Sheet 01 -A_Goals_of_Victory

---
+++ Codeforces Problem/Sheet 01/A_Goals_of_Victory.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin >>t;
+    while (t--) {
+        int n;
+        cin >>n;
+        long long sum=0;
+        for (int i=0;i<n-1;i++) {
+            int a;
+            cin >>a;
+            sum+=a;
+        }
+        cout << -sum <<endl;
+    }
+    return 0;
+}

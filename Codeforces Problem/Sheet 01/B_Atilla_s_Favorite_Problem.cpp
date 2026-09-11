5fbd8422ab65f5c9b94602a32085a9aa1de4de15
Hamim__Hasan <hamimlimon12@gmail.com>
2026-09-11 18:53:01 +0600

    Sheet 01 -B_Atilla_s_Favorite_Problem

---
+++ Codeforces Problem/Sheet 01/B_Atilla_s_Favorite_Problem.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+    while (t--){
+        int n;
+        cin>>n;
+        char s[105];
+        cin >>s;
+        char mx='a';
+        for(int i=0;i<n;i++) {
+            mx=max(mx,s[i]);
+        }
+        cout<<mx-'a'+1<<endl;
+    }
+    return 0;
+}
